7e397a557339c6c714c8d69909999b0bcda2ce1d
goreleaserbot <goreleaser@carlosbecker.com>
2022-10-17 08:30:27 +0200

    Brew formula update for app-package version v0.1.0-rc.6

--- Formula/app-package.rb
+++ Formula/app-package.rb
@@ -5,20 +5,20 @@
 class AppPackage < Formula
   desc "app-package official Homebrew distribution!"
   homepage "https://github.com/mas2020-golang/app-package"
-  version "0.1.0-rc.5"
+  version "0.1.0-rc.6"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/mas2020-golang/app-package/releases/download/v0.1.0-rc.5/app-package_v0.1.0-rc.5_Darwin-arm64.tar.gz"
-      sha256 "9f490e00cf20572a6d3afeb9ec039e4faf2d2ed050f4f808b25d20e5c1bde231"
+      url "https://github.com/mas2020-golang/app-package/releases/download/v0.1.0-rc.6/app-package_v0.1.0-rc.6_Darwin-arm64.tar.gz"
+      sha256 "6ab8d91fe441aac6dcb343bdbf92307dbe5bf46b88d0bd5a87e588fc56a56dad"
 
       def install
         bin.install "app-package"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/mas2020-golang/app-package/releases/download/v0.1.0-rc.5/app-package_v0.1.0-rc.5_Darwin-x86_64.tar.gz"
-      sha256 "8a3ec2d3181f37fd6abc274eef2151feb2d32241170a600c57612629294c2343"
+      url "https://github.com/mas2020-golang/app-package/releases/download/v0.1.0-rc.6/app-package_v0.1.0-rc.6_Darwin-x86_64.tar.gz"
+      sha256 "c46d0737936eb4568e1b225e618bfc7a910ce9693392512a2b7876d3446c1a62"
 
       def install
         bin.install "app-package"
@@ -28,16 +28,16 @@ class AppPackage < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mas2020-golang/app-package/releases/download/v0.1.0-rc.5/app-package_v0.1.0-rc.5_Linux-arm64.tar.gz"
-      sha256 "f63a8ee69528df0255c607ba2e352c114a34f582c822ae50428c5865cfadc048"
+      url "https://github.com/mas2020-golang/app-package/releases/download/v0.1.0-rc.6/app-package_v0.1.0-rc.6_Linux-arm64.tar.gz"
+      sha256 "4c327cfbbf909d2967ce8965e757eee6921b663d5902364b2e4aca3432c273f6"
 
       def install
         bin.install "app-package"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/mas2020-golang/app-package/releases/download/v0.1.0-rc.5/app-package_v0.1.0-rc.5_Linux-x86_64.tar.gz"
-      sha256 "d872fc1d932d4aeddb1301cdaec752c2a6a1e32b68ffeda3817b77f09e2cd474"
+      url "https://github.com/mas2020-golang/app-package/releases/download/v0.1.0-rc.6/app-package_v0.1.0-rc.6_Linux-x86_64.tar.gz"
+      sha256 "1cf047ee8cdab97d27b180ee6b2bcf2f828f9a6f3a655d134cabcbaefc7f6438"
 
       def install
         bin.install "app-package"
